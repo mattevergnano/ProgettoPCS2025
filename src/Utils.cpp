8257a7886553d6882e6b7e697c6b3a7e726c1ed1
Noemi <s299186@studenti.polito.it>
2025-05-23 09:23:44 +0200

    ho aggiunto i calori della cella 0 e 1

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -49,7 +49,8 @@ namespace PlatonicLibrary{
             cout << "Tetraedro" << endl;
             solido.NumCells0Ds = 4;
             solido.Cells0DsId = vector<unsigned int>(solido.NumCells0Ds,0);
-            for(unsigned int i=0;i<solido.NumCells0Ds;i++)
+            solido.NumCells1Ds=6;
+			for(unsigned int i=0;i<solido.NumCells0Ds;i++)
                 solido.Cells0DsId[i] = i;
             solido.Cells0DsCoordinates = MatrixXd::Zero(solido.NumCells0Ds,3);
             solido.Cells0DsCoordinates << 0.0,0.0,1.0,
@@ -58,9 +59,12 @@ namespace PlatonicLibrary{
                                         0.0,0.0,-1.0;
         }else if(solido.p == 4 && solido.q == 3){
             cout << "Cubo" << endl;
+			solido.NumCells0Ds=8;
+			solido.NumCells1Ds=12;
         }else if(solido.p == 3 && solido.q == 4){
             cout << "Ottaedro" << endl;
             solido.NumCells0Ds = 6;
+			solido.NumCells1Ds=12;
             solido.Cells0DsId = vector<unsigned int>(solido.NumCells0Ds,0);
             for(unsigned int i=0;i<solido.NumCells0Ds;i++)
                 solido.Cells0DsId[i] = i;
@@ -73,10 +77,13 @@ namespace PlatonicLibrary{
                                           0.0,0.0,-1.0;
         }else if(solido.p == 5 && solido.q == 3){
             cout << "Dodecaedro" << endl;
+			solido.NumCells0Ds=20;
+			solido.NumCells1Ds=30;
         }else if(solido.p == 3 && solido.q == 5){
             cout << "Icosaedro" << endl;
             double phi = (1+sqrt(5))/2;
             solido.NumCells0Ds = 12;
+			solido.NumCells1Ds=30;
             solido.Cells0DsId = vector<unsigned int>(solido.NumCells0Ds,0);
             for(unsigned int i=0;i<solido.NumCells0Ds;i++)
                 solido.Cells0DsId[i] = i;
